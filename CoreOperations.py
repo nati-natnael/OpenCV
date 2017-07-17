b31b4b7e0713a4a64b2cb15da04f302f09ef2f07
nati-natnael <natnaelger@gmail.com>
2017-07-17 09:16:16 -0500

    Logo Mask

--- CoreOperations.py
+++ CoreOperations.py
@@ -14,12 +14,15 @@ logo = cv2.imread("C:\Users\Public\Pictures\Sample Pictures\opencv.png", cv2.IMR
 
 # res = cv2.addWeighted(img, 0.2, img2, 0.2, 0)
 
+logo = cv2.cvtColor(logo, cv2.COLOR_RGBA2BGR)
 rows, cols, channels = logo.shape
 roi = img[0:rows, 0:cols]
 
+roi_rows, roi_cols, roi_channels = roi.shape
+
 img2gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
 ret, mask = cv2.threshold(img2gray, 10, 255, cv2.THRESH_BINARY)
-mask_inv = cv2.bitwise_and(roi, roi, mask = mask)
+mask_inv = cv2.bitwise_not(mask)
 
 img1_bg = cv2.bitwise_and(roi, roi, mask = mask_inv)
 img2_fg = cv2.bitwise_and(logo, logo, mask = mask)
