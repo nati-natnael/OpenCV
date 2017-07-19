7a57469e293d1157c91693d64b592decb0dbb139
nati-natnael <natnaelger@gmail.com>
2017-07-19 16:00:00 -0500

    Drew a contour over an object

--- Tutorials/Contours.py
+++ Tutorials/Contours.py
@@ -1,13 +1,23 @@
 import cv2
 import numpy as np
 
-img = cv2.imread("..\\files\\white_square.jpg", cv2.COLOR_BGR2GRAY)
-ret, th = cv2.threshold(img, 127, 255, 0)
-image, contours, hierarcy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+img = cv2.imread("..\\files\\square.jpg")
 
-img = cv2.drawContours(img, contours, 0, (0,255,0), 3)
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, th = cv2.threshold(imgray, 127, 255, 0)
+image, contours, hierarchy = cv2.findContours(th, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.imshow('image', th)
+img = cv2.drawContours(img, contours, 1, (0, 255, 0), 5)
+#
+# cnt = contours[0]
+# M = cv2.moments(cnt)
+# print M
+# print M['m00']  # Area
+
+# perimeter = cv2.arcLength(cnt, True)
+# print perimeter
+
+cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
