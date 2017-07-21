357f4c8d7bf54dd0dd863f9ccacc549c78ae42f6
nati-natnael <natnaelger@gmail.com>
2017-07-21 15:52:27 -0500

    Positive image and descriptor creator

--- ImageDetection/TrainerDataRetriever.py
+++ ImageDetection/TrainerDataRetriever.py
@@ -1,15 +1,19 @@
 import cv2
 import os
 import numpy as np
+import sys
 from urllib import urlopen
 from urllib import urlretrieve
 
 
 class TrainerDataRetriever(object):
     def __init__(self):
+        self.clicked = False
+        self.start = None
+        self.dimension = None
         pass
 
-    def retrieveLink(*args):
+    def pullFiles(*args):
         """
         Download files using the online image links included
         in img_path 'links'. when downloading images
@@ -23,7 +27,7 @@ class TrainerDataRetriever(object):
         :return:
         """
 
-        if len(args) < 4:
+        if len(args) < 5:
             raise Exception("Illegal Argument Count")
 
         links = args[1]
@@ -34,10 +38,10 @@ class TrainerDataRetriever(object):
         img_size = (100, 100)
 
         # Parse Args
-        if len(args) > 6:
+        if len(args) == 6:
             how_many = int(args[5])
 
-        elif len(args) >= 7:
+        elif len(args) == 7:
             how_many = int(args[5])
             img_size = args[6]
 
@@ -55,7 +59,7 @@ class TrainerDataRetriever(object):
             for i in url.split('\n'):
                 try:
                     print i
-                    img_path = dir + str(file_num) + file_type
+                    img_path = dir + "img" + str(file_num) + file_type
                     urlretrieve(i, img_path)
                     img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
                     img = cv2.resize(img, img_size)
@@ -106,8 +110,9 @@ class TrainerDataRetriever(object):
                 except Exception as e:
                     print("Error: " + str(e))
 
-    def neg_imgs_descriptor(self, descriptor_path, imgs_path):
+    def negative(self, descriptor_path, imgs_path):
         """
+        Make a descriptor txt file for data sets
 
         :param descriptor_path: image descriptor txt file path
         :param imgs_path: directory where images live
@@ -123,10 +128,80 @@ class TrainerDataRetriever(object):
 
         f.close()
 
+    def mouse_clicked(self, event, x, y, flags, param):
+        """
+
+        :param event:
+        :param x:
+        :param y:
+        :param flags:
+        :param param:
+        :return:
+        """
+        if event == cv2.EVENT_LBUTTONDOWN:
+            self.clicked = True
+            self.start = (x, y)
+            print("Left Mouse Button clicked: (x, y) => " + str(x) + ", " + str(y))
+
+        elif event == cv2.EVENT_LBUTTONUP:
+            print("Left Mouse Button Relesed")
+            self.clicked = False
+            self.start = None
+            self.dimension = None
+
+        elif event == cv2.EVENT_MOUSEMOVE and self.clicked:
+            self.dimension = (x, y)
+            print("Mouse Dragged: (x, y) => " + str(self.dimension))
+
+        pass
+
+    def mouse_dragged(self, event, x, y, flags, param):
+        if event == cv2.EVENT_MOUSEMOVE and self.clicked:
+            print("mouse Moved")
+        pass
+
+    def positive(self, desciptor_path, imgs_path):
+        """
+
+        :param desciptor_path:
+        :param imgs_path:
+        :return:
+        """
+
+        crop_color = (0, 255, 0)
+
+        for img in os.listdir(imgs_path):
+            path = imgs_path + img
+            image = cv2.imread(path)
+
+            cv2.imshow('image', image)
+            cv2.setMouseCallback('image', self.mouse_clicked)
+
+            while True:
+                cv2.imshow('image', image)
+                key = cv2.waitKey(100)
+
+                if self.start is not None:
+                    cv2.rectangle(image, self.start, self.dimension, crop_color)
+                    cv2.imshow('image', image)
+
+                if key == ord(' '):
+                    print("Going to next image")
+                    self.start = None
+                    self.dimension = None
+                    break
+
+                elif key == 27:
+                    print("Exiting")
+                    sys.exit(0)
+
+        cv2.destroyAllWindows()
+
 
 # Test
 link = 'http://image-net.org/api/text/imagenet.synset.geturls?wnid=n09618957'
 m = TrainerDataRetriever()
-# m.retrieveLink(link, "..\\files\\haar_cascades\\images\\", '.jpg', 1, 100, (200, 200))
+# m.pullFiles(link, "..\\files\\haar_cascades\\images\\", '.jpg', 1, 20, (200, 200))
 # m.removeBadImg("..\\files\\haar_cascades\\images\\", "..\\files\\haar_cascades\\bad_imgs\\")
-m.neg_imgs_descriptor("..\\files\\haar_cascades\\Training\\neg\\bg.txt", "..\\files\\haar_cascades\\images\\")
+# m.negative("..\\files\\haar_cascades\\Training\\neg\\bg.txt", "..\\files\\haar_cascades\\images\\")
+m.positive("..\\files\\haar_cascades\\Training\\neg\\bg.txt", "..\\files\\haar_cascades\\images\\")
