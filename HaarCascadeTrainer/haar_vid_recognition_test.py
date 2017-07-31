70cf4d1ca316033aa4011b2daf75cff1d8323aab
nati-natnael <natnaelger@gmail.com>
2017-07-31 15:40:24 -0500

    made a general command executor

--- HaarCascadeTrainer/haar_vid_recognition_test.py
+++ HaarCascadeTrainer/haar_vid_recognition_test.py
@@ -4,14 +4,15 @@ cascade_dir = 'C:/Users/uc212807/PycharmProjects/OpenCV/HaarCascadeTrainer/haar_
 cascade = 'cascade.xml'
 face = cv2.CascadeClassifier(cascade_dir + cascade)
 
-cap = cv2.VideoCapture('C:/Users/uc212807/Desktop/VIDEO0002.avi')
+cap = cv2.VideoCapture('C:/Users/uc212807/Desktop/vid.mp4')
 
-while cap:
+while cap.isOpened():
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.resize(gray, (500, 500))
     faces = face.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in faces:
         img = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
@@ -19,7 +20,8 @@ while cap:
         roi_color = img[y:y + h, x:x + w]
 
     # Display the resulting frame
-    frame = cv2.flip(frame, 1)
+    # frame = cv2.flip(frame, 1)
+    frame = cv2.resize(frame, (500, 500))
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
